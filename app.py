97ad333dcb4cf66f6d5aa185e0c4a4e48678ff25
HugoCol <55191730+HugoCol@users.noreply.github.com>
2020-05-14 20:38:40 +0200

    Update app.py
    update voor committen

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template,request
 from dna_to_protein import translate
+from fastafilereader import fastafile_reader
 
 app = Flask(__name__)
 
@@ -12,7 +13,18 @@ def index():
         protein = translate(seq)
         return render_template('hello_world.html', title='Home',proteins=protein)
     else:
-        return render_template('hello_world.html', title='Home',proteins='get_method')
+        return render_template('hello_world.html', title='Home',proteins='protein',fastafile='fastafile')
+
+@app.route('/fastafile',methods=["POST", "GET"])
+def fastafile():
+
+    if request.method == "POST":
+        fastafile = request.form.get("fastafile", "")
+        print(fastafile)
+        fasta_output = fastafile_reader(fastafile)
+        return render_template('hello_world.html', title='Home',proteins='',fastafile=fasta_output)
+    else:
+        return render_template('hello_world.html', title='Home',proteins='',fastafile='fastafile')
 
 if __name__ == '__main__':
     app.run()
