e48616b259b9cdab3f84f03e34afcd33d7c85636
Ivar2001 <iaoqi@student.han.nl>
2020-05-19 16:26:47 +0200

    File Updated 1.1 16:26

--- BlastToDatabase.py
+++ BlastToDatabase.py
@@ -1,71 +1,72 @@
-import mysql.connector
-
-
 def getdata():
 
-    return funcdic,
-def pushdata():
+    return datadic
+
+
+def pushdata(datadic):
     print("Connecting to database.....")
     conn = mysql.connector.connect(
                     host="hannl-hlo-bioinformatica-mysqlsrv.mysql.database."
                          "azure.com",
                     user="iaoqi@hannl-hlo-bioinformatica-mysqlsrv",
                     db="iaoqi", password="638942")
+    conn = mysql.connector.connect(
+        host="sql2.freemysqlhosting.net",
+        user="sql234812",
+        db="sql234812", password="iM4*eV6%")
     print("Connected")
     for i in datadic:
-        if i != "":
+        if i[] != "":
             cursor = conn.cursor()
-            cursor.execute(f"insert into functies (functie_eiwit) "
-                           f"values ();")
+            cursor.execute(f"insert into lineage (name, parent_id) "
+                           f"values (i[]);")
             conn.commit()
-            print("Functietabel fill completed")
-        if i != "":
-            cursor = conn.cursor()
-            cursor.execute(f"insert into sequences (seq_read1, seq_read2) "
-                           f"values ();")
-            conn.commit()
-            print("Sequences table fill completed")
-        if i != "":
-            cursor = conn.cursor()
-            cursor.execute(f"insert into soort (soortnaam) "
-                           f"values ();")
-            conn.commit()
-            print("Soort table fill completed")
-        if i != "":
-            cursor = conn.cursor()
-            cursor.execute(f"insert into geslacht (geslacht_naam) "
-                           f"values ();")
-            conn.commit()
-            print("Geslacht table fill completed")
-        if i != "":
-            cursor = conn.cursor()
-            cursor.execute(f"insert into familie (familie_naam) "
-                           f"values ();")
-            conn.commit()
-            print("Familie table fill completed")
-        if i != "":
-            cursor = conn.cursor()
-            cursor.execute(f"insert into gegevens (max_score, total_score, "
-                           f"query_cover, percent_identity, e_value) "
-                           f"values ();")
-            conn.commit()
-            print("Gegevens table fill completed")
-        if i != "":
-            cursor = conn.cursor()
-            cursor.execute(f"insert into eiwit (eiwit_naam, familie_id, "
-                           f"geslacht_id, soort_id, seq_id, functie_id, gegevens_id) "
-                           f"values ();")
-            conn.commit()
-            print("Eiwit table fill completed")
+            print("lineage fill for ",i[]," completed")
+        if i[] != "":
+            cursorchecko = conn.cursor()
+            cursor.execute(f"select naam_organisme from organisme where naam_organisme = i[]")
+            if cursorchecko == "":
+                linidcursor = conn.cursor()
+                cursor.execute(f"select id from lineage where name = i[]")
+                cursor = conn.cursor()
+                cursor.execute(f"insert into organisme (naam_organisme lineage_id, eiwit_id) "
+                               f"values (i[]}, linidcursor, i[]);")
+                conn.commit()
+                print("Organisme fill for ",i[]," completed")
+        if i[] != "":
+            cursorchecks = conn.cursor()
+            cursor.execute(f"select header from sequentie where header = i[]")
+            if cursorchecks == "":
+                cursor = conn.cursor()
+                cursor.execute(f"insert into sequentie (header, sequence, "
+                               f"asci_score, 'read') values (i[], i[], i[], i[]);")
+                conn.commit()
+                print("Gegevens fill for ",i[]," completed")
+                cursorchecks += ""
+        if i[] != "":
+            seqidcursor = conn.cursor()
+            cursor.execute(f"select id from sequentie where header = i[]")
+            orgidcursor = conn.cursor()
+            cursor.execute(f"select id from organisme where naam_organisme = i[]")
 
+            cursor = conn.cursor()
+            cursor.execute(f"insert into eiwit (description, accessiecode, "
+                           f"percent_identity, e_value, max_score, total_score,"
+                           f"query_cover sequentie_id, Organisme_id) "
+                           f"values (i[], i[], i[], i[], i[], i[],"
+                           f"i[], seqidcursor, orgidcursor);")
+            conn.commit()
+            print("Eiwit  fill for ",i[]," completed")
+    print("Database filled!\nClosing connection....")
     cursor.close()
     conn.close()
+    print("Connection closed task compleded.")
     return
 
-if __name__ == '__main__':
-    pushdata()
-
-
+def main():
+    datadic = getdata()
+    pushdata(datadic)
+main()
 # Lijst met data pet 5 de if for loop doen
 # in de for loop op volgorde van tabbellen gaan vullen
 # Bij de tabbellen die dubbele data kunnen bevatten eerst een if statement
