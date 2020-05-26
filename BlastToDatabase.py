54e7d501da290e4422e3e7ac37b5367ca2772018
Ivar2001 <iaoqi@student.han.nl>
2020-05-26 17:23:05 +0200

    File Updated 1.1 16:26

--- BlastToDatabase.py
+++ BlastToDatabase.py
@@ -2,13 +2,14 @@
 # 18-05-2020 tot 27-05-2020
 # Script dat heel veel data uit 2 bestanden en 1 database haalt deze
 # in een dictionary zet en met de dictionary de database vult
-
+import mysql.connector
 from xml.etree import ElementTree
 import time
 from Bio import Entrez
+import os
 
 
-def getdata():
+def getdata(datafile):
     """
     :return:
     """
@@ -22,7 +23,6 @@ def getdata():
     count = 1
     datadic = {}
     mestdatadic = {}
-    datafile = 'data/dataset.txt'
     file = open(datafile)
     fwcount = 1
     revcount = 2
@@ -97,17 +97,17 @@ def pushdata(datadic):
     # Connectie met database maken
     print("Connecting to database.....")
     # Inlog gegevens voor Azure database (nette database)
-    # conn = mysql.connector.connect(
-    #                 host="hannl-hlo-bioinformatica-mysqlsrv.mysql.database."
-    #                      "azure.com",
-    #                 user="iaoqi@hannl-hlo-bioinformatica-mysqlsrv",
-    #                 db="iaoqi", password="638942")
-    # Inlog gegevens voor oefen database (Hier het script op testen)
     conn = mysql.connector.connect(
-        host="sql2.freemysqlhosting.net",
-        user="sql234812",
-        db="sql234812", password="iM4*eV6%")
-    print("Connected")
+                    host="hannl-hlo-bioinformatica-mysqlsrv.mysql.database."
+                         "azure.com",
+                    user="iaoqi@hannl-hlo-bioinformatica-mysqlsrv",
+                    db="iaoqi", password="638942")
+    # Inlog gegevens voor oefen database (Hier het script op testen)
+    # conn = mysql.connector.connect(
+    #     host="sql2.freemysqlhosting.net",
+    #     user="sql234812",
+    #     db="sql234812", password="iM4*eV6%")
+    # print("Connected")
     # Voor elke regel in de dictionary waar de data in staat
     for i in datadic:
         countlin = 0
@@ -115,22 +115,33 @@ def pushdata(datadic):
         if datadic[i][11] != "":
             for j in datadic[i][11]:
                 print(j)
-                if countlin == 0:
+                lincheck = conn.cursor()
+                lincheck.execute(f"select name from lineage where name = j")
+                conn.commit()
+                cursor.close()
+                if lincheck == "":
+                    if countlin == 0:
+                        cursor = conn.cursor()
+                        cursor.execute(f"insert into lineage (name, parent_id) "
+                                       f"values (datadic[i][11][countlin]);")
+                        conn.commit()
+                        cursor.close()
+                else:
+                    formercount = countlin - 1
+                    # Vul de lineage tabel met data
+                    linid = conn.cursor()
+                    linid.execute(f"select id from lineage where name = datadic[i][11][formercount]")
+                    conn.commit()
+                    cursor.close()
                     cursor = conn.cursor()
                     cursor.execute(f"insert into lineage (name, parent_id) "
-                                   f"values (datadic[i][11][countlin]);")
+                                   f"values (datadic[i][11][countlin], linid);")
                     conn.commit()
+                    cursor.close()
+                    countlin += 1
             else:
-                formercount = countlin - 1
-                # Vul de lineage tabel met data
-                linid = conn.cursor()
-                cursor.execute(f"select id from lineage where name = "
-                               f"datadic[i][11][formercount]")
-                cursor = conn.cursor()
-                cursor.execute(f"insert into lineage (name, parent_id) "
-                               f"values (datadic[i][11][countlin], linid);")
-                conn.commit()
-                countlin += 1
+                print("Item", j,"Bestaat al")
+                pass
             print("lineage fill for ", datadic[i][3], " completed")
             # Kijk of de plek gevult is met data
         if datadic[i][2] != "":
@@ -138,19 +149,27 @@ def pushdata(datadic):
             cursorchecko = conn.cursor()
             cursor.execute(
                 f"select naam_organisme from organisme where naam_organisme = datadic[i][]")
+            conn.commit()
+            cursor.close()
             # Als de variabele leeg blijft data in de database zetten
             if cursorchecko == "":
                 # Het id van linage ophalen om deze in de organisme tabel te zetten
                 linidcursor = conn.cursor()
                 cursor.execute(
                     f"select id from lineage where name = datadic[i][11][-1]")
+                conn.commit()
+                cursor.close()
+
+
                 cursor = conn.cursor()
                 cursor.execute(
                     f"insert into organisme (naam_organisme lineage_id, eiwit_id) "
                     f"values (datadic[i][2], linidcursor, datadic[i][0]);")
                 conn.commit()
+                cursor.close()
                 print("Organisme fill for ", datadic[i][3], " completed")
             else:
+                print(cursorchecko, "bestaat al")
                 pass
             cursorchecko += ""
         # Kijk of de plek gevult is met data
@@ -158,14 +177,18 @@ def pushdata(datadic):
             # Kijken of de data die in de database gaat er al in staat
             cursorchecks = conn.cursor()
             cursor.execute(f"select header from sequentie where header = datadic[i][9]")
+            conn.commit()
+            cursor.close()
             # Als de variabele leeg blijft data in de database zetten
             if cursorchecks == "":
                 cursor = conn.cursor()
                 cursor.execute(f"insert into sequentie (header, sequence, "
                                f"asci_score, read) values (datadic[i][9], datadic[i][8], datadic[i][10], 1);")
                 conn.commit()
+                cursor.close()
                 print("Gegevens fill for ", datadic[i][3], " completed")
             else:
+                print(cursorchecks, "bestaat al")
                 pass
             cursorchecks += ""
         # Kijk of de plek gevult is met data
@@ -174,10 +197,14 @@ def pushdata(datadic):
             seqidcursor = conn.cursor()
             cursor.execute(
                 f"select id from sequentie where header = datadic[i][9]")
+            conn.commit()
+            cursor.close()
             # Het id van organisme ophalen om deze in de organisme tabel te zetten
             orgidcursor = conn.cursor()
             cursor.execute(
                 f"select id from organisme where naam_organisme = datadic[i][2]")
+            conn.commit()
+            cursor.close()
             # De eiwit tabel vullen met data
             cursor = conn.cursor()
             cursor.execute(f"insert into eiwit (description, accessiecode, "
@@ -186,18 +213,19 @@ def pushdata(datadic):
                            f"values (datadic[i][1], datadic[i][3], datadic[i][7], datadic[i][6], datadic[i][4], datadic[i][5],"
                            f"datadic[i][], seqidcursor, orgidcursor);")
             conn.commit()
+            cursor.close()
             print("Eiwit  fill for ", datadic[i][3], " completed")
     print("Database filled!\nClosing connection....")
     # Connectie met database sluiten
-    cursor.close()
     conn.close()
     print("Connection closed task compleded.")
     return
 
 
 def main():
-    datadic = getdata()
-    # pushdata(datadic)
+    datafile = 'data/dataset.txt'
+    datadic = getdata(datafile)
+    pushdata(datadic)
 
 
 main()
