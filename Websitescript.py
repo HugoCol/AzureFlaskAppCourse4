043f1ecf0fc50a53343ee5eb843b613eca26ec10
Ivar2001 <iaoqi@student.han.nl>
2020-06-02 17:49:16 +0200

    File Updated 1.1 16:26

--- Websitescript.py
+++ Websitescript.py
@@ -2,7 +2,7 @@
 # SERCHBAR: dit is het word waarop gezocht woord
 import mysql.connector
 
-def zoeken():
+def zoeken(filter, search):
     msg = {}
     conn = mysql.connector.connect(
                     host="hannl-hlo-bioinformatica-mysqlsrv.mysql.database."
@@ -38,7 +38,7 @@ def zoeken():
                            f"lineage.name as linnaam from organisme join lineage on "
                            f"organisme.lineage_id=lineage.id) as orjoin on "
                            f"eiwit.Organisme_id=orjoin.id join sequentie s on "
-                           f"eiwit.sequentie_id = s.id where '{filter}'='"
+                           f"eiwit.sequentie_id = s.id where description ='"
                            f"{search}'order by eiwit.id ASC limit 500;")
             for i in cursor:
                 msg[f"{i[0]}"] = values[f"{i[1]} {i[2]} {i[3]} {i[4]} {i[5]}" \
@@ -46,6 +46,7 @@ def zoeken():
                                         f"{i[10]} {i[11]}"]
             cursor.close()
             conn.close()
+
         else:
             cursor = conn.cursor()
             cursor.execute(f"select eiwit.id, description, accessiecode, "
@@ -56,7 +57,7 @@ def zoeken():
                            f"lineage.name as linnaam from organisme join lineage on "
                            f"organisme.lineage_id=lineage.id) as orjoin on "
                            f"eiwit.Organisme_id=orjoin.id join sequentie s on "
-                           f"eiwit.sequentie_id = s.id where description ='"
+                           f"eiwit.sequentie_id = s.id where '{filter}'='"
                            f"{search}'order by eiwit.id ASC limit 500;")
             for i in cursor:
                 msg[f"{i[0]}"] = values[f"{i[1]} {i[2]} {i[3]} {i[4]} {i[5]}" \
@@ -64,4 +65,5 @@ def zoeken():
                                         f"{i[10]} {i[11]}"]
             cursor.close()
             conn.close()
+
     return msg
