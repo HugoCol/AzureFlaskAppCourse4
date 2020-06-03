5ef3d47229ceb304b1936cd784d59a4bfdef749f
HugoCol <hugo.colenbrander@gmail.com>
2020-06-03 13:20:54 +0200

    Websitescript.py shit uitgewerkt

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
 from flask import Flask, render_template, request
-import mysql.connector
 from Websitescript import zoeken
 
 
 app = Flask(__name__)
 
 
-host = "hannl-hlo-bioinformatica-mysqlsrv.mysql.database.azure.com"
-user = "iaoqi@hannl-hlo-bioinformatica-mysqlsrv"
-password = "638942"
-db = "iaoqi"
 
 @app.route('/')
 @app.route('/protein', methods=["POST", "GET"])
@@ -25,29 +20,7 @@ def database():
         print(zoekopdracht)
 
         resultatenlijst = zoeken(buttonselect,zoekopdracht)
-        # if buttonselect == 'organisme':
-        #     column = 'naam_organismenaam'
-        #
-        # elif buttonselect == 'eiwit':
-        #     column = 'description'
-        #
-        # elif buttonselect == 'accesiecode':
-        #     column = 'accessiecode'
-        #
-        # elif buttonselect == 'lineage':
-        #     column = 'name'
-        #
-        # conn = mysql.connector.connect(host=host,user=user,password=password,db=db)
-        # cursor = conn.cursor()
-        # query = "select * from " + buttonselect + " where "+ column + " like \"%" + zoekopdracht \
-        #         + "%\""
-        # cursor.execute(query)
-        #
-        # for rij in cursor:
-        #     resultatenlijst.append(rij)
-        #     print(rij)
-        # cursor.close()
-        # conn.close()
+
 
         rangeresultatentext = "Alleen de eerste 20 resultaten worden " \
                               "weergegeven. Dit kan worden aangepast " \
