51bdf69f062aca2fb681da38f2b3b20dc46f83d7
Ivar2001 <iaoqi@student.han.nl>
2020-06-09 14:42:11 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	templates/home.html

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from Bio.Blast import NCBIWWW, NCBIXML
 from xml.etree import ElementTree
 import time
 from Bio import Entrez
+import mysql.connector
 
 # app aanroepen
 app = Flask(__name__)
@@ -105,6 +106,15 @@ def blast():
     input: DNA-sequentie
     output: Blast resultaat en toevoeging in de database
     """
+    # SELECT id FROM eiwit ORDER BY id DESC LIMIT 1;
+    # SELECT id FROM sequentie ORDER BY id DESC LIMIT 1;
+    # SELECT id FROM organisme ORDER BY id DESC LIMIT 1;
+    # SELECT id FROM lineage ORDER BY id DESC LIMIT 1;
+    conn = mysql.connector.connect(
+             host="hannl-hlo-bioinformatica-mysqlsrv.mysql.database."
+                  "azure.com",
+             user="iaoqi@hannl-hlo-bioinformatica-mysqlsrv",
+             db="iaoqi", password="638942")
     if request.method == "POST":
         geenresultaten = ""
         # Gegevens uit textbox halen en dit in hoofdletters zetten.
@@ -135,7 +145,6 @@ def blast():
                 out_handle.write(result_handle.read())
 
             XMLFile = "XMLBlastWebsite.xml"
-            datafile = 'dataset.txt'
 
             # Door bestand heen gaan
             dom = ElementTree.parse(XMLFile)
@@ -143,26 +152,20 @@ def blast():
             hits = dom.findall(
                 'BlastOutput_iterations/Iteration/Iteration_hits/Hit')
             # Voor elke hit
-            count = 1
             datadic = {}
-            mestdatadic = {}
-            file = open(datafile)
-            fwcount = 1
-            revcount = 2
-            keycounter = 0
-            for line in file:
-                mestdata = line.split('\t')
-                mestdatadic[fwcount] = [mestdata[0], mestdata[1],
-                                        mestdata[2]]
-                mestdatadic[revcount] = [mestdata[3], mestdata[4],
-                                         mestdata[5]]
-                fwcount += 2
-                revcount += 2
             for c in hits:
+                cursor = conn.cursor()
+                string2 = f"SELECT id FROM eiwit ORDER BY id DESC LIMIT 1"
+                cursor.execute(string2)
+                conn.commit()
+                eiwitid = cursor.fetchall()
+                replacer = str(eiwitid[0])
+                coreid = replacer.replace('(', "").replace \
+                    (',', "").replace(')', "")
+                cursor.close()
                 if count <= 50:
                     # Haal de data uit de hit en zet deze in een zelf
                     # beschrijvende variabele
-                    Hit_id = c.find('Hit_id').text
                     discript = c.find('Hit_def').text
                     totaallengte = c.find('Hit_len').text
                     querybegin = c.find(
@@ -172,20 +175,17 @@ def blast():
                         querybegin)) / int(
                         totaallengte)
                     organis = discript.split('[')
-                    organism = organis[1].split(']')
-                    description = organis[0]
-                    organisme = organism[0]
+                    organisme = organis[-1].strip(']')
                     acessiecode = c.find('Hit_accession').text
                     print(acessiecode)
-                    hit = c.find('Hit_num').text
                     score = c.find('Hit_hsps/Hsp/Hsp_bit-score').text
                     tscore = c.find('Hit_hsps/Hsp/Hsp_score').text
                     evalue = c.find('Hit_hsps/Hsp/Hsp_evalue').text
                     percidentity = \
                         c.find('Hit_hsps/Hsp/Hsp_identity').text
                     queryseq = c.find('Hit_hsps/Hsp/Hsp_qseq').text
-                    header = ""
-                    ascicode = ""
+                    header = "Website Blast"
+                    ascicode = "Null"
                     time.sleep(0.4)
                     Entrez.email = "thijschermens@gmail.com"
                     seqio = Entrez.efetch(db="protein", id=acessiecode,
@@ -193,30 +193,158 @@ def blast():
                     seqio_read = Entrez.read(seqio)
                     seqio.close()
                     lineage = seqio_read[0]["GBSeq_taxonomy"].split(";")
-                    if hit == str(1):
-                        keycounter += 1
-                        for (key, value) in mestdatadic.items():
-                            if key == keycounter:
-                                nbheader = str(value[0])
-                                hbheader = nbheader.split('@')
-                                header += hbheader[1]
-                                ascicode += str(value[2])
-
-                    else:
-                        for (key, value) in mestdatadic.items():
-                            if key == keycounter:
-                                nbheader = str(value[0])
-                                hbheader += nbheader.split('@')
-                                header += hbheader[1]
-                                ascicode += str(value[2])
-
+                    eiwitid = coreid + 1
                     # Print alle data om te zien of het gewerkt heeft
-                    datalist = [Hit_id, description, organisme,
+                    datalist = [description, organisme,
                                 acessiecode, score, tscore, evalue,
                                 percidentity, queryseq, header,
                                 ascicode, lineage, querycoverage]
-                    datadic[count] = datalist
-                    count += 1
+                    datadic[eiwitid] = datalist
+                    for i in datadic:
+                        countlin = 0
+                        for j in datadic[i][10]:
+                            lincheck = conn.cursor(buffered=True)
+                            string1 = f"select name from lineage where name = '{j}'"
+                            lincheck.execute(string1)
+                            conn.commit()
+                            test = lincheck.fetchall()
+                            lincheck.close()
+
+                            lineageid = f"SELECT id FROM lineage ORDER BY id DESC LIMIT 1"
+                            cursor.execute(lineageid)
+                            conn.commit()
+                            lineagelastid = cursor.fetchall()
+                            replacer1 = str(lineagelastid[0])
+                            replace2 = replacer1.replace('(', "").replace \
+                                (',', "").replace(')', "")
+                            cursor.close()
+                            teller1 = replace2 + 1
+                            if test == []:
+                                if countlin != 0:
+                                    string2 = f"insert into lineage (id, name) values " \
+                                              f"('{teller1}', " \
+                                              f"'{datadic[i][10][countlin]}')"
+                                    cursor.execute(string2)
+                                    conn.commit()
+                                    cursor.close()
+                                else:
+                                    formercount = countlin - 1
+                                    string3 = f"select parent_id from lineage where name " \
+                                              f"= '{datadic[i][10][formercount]}'"
+                                    linid.execute(string3)
+                                    conn.commit()
+                                    pliniageid = linid.fetchall()
+                                    replacer = str(pliniageid[0])
+                                    replace1 = replacer.replace('(', "").replace\
+                                        (',', "").replace(')', "")
+                                    linid.close()
+                                    string4 = f"insert into lineage (id, name, " \
+                                              f"parent_id) values ('{teller1}', " \
+                                              f"'{datadic[i][10][countlin]}', " \
+                                              f"'{replace1}')"
+                                    cursor.execute(string4)
+                                    conn.commit()
+                                    cursor.close()
+                                    countlin += 1
+                            else:
+                                pass
+                        cursor = conn.cursor()
+                        linidcursor = conn.cursor(buffered=True)
+                        string6 = f"select id from lineage where name " \
+                                  f"= '{datadic[i][10][-1]}'"
+                        linidcursor.execute(string6)
+                        conn.commit()
+                        # Het id prepareren zodat het alleen een cijfer is
+                        # ipv een lijst met cursor hits
+                        orglin = linidcursor.fetchall()
+                        linidcursor.close()
+                        replacer2 = str(orglin[0])
+                        replace3 = replacer2.replace('(', "").replace(',',
+                                                                      "").replace(
+                            ')', "")
+
+
+                        cursor = conn.cursor()
+                        organismelastid = f"SELECT id FROM organisme ORDER BY id DESC LIMIT 1"
+                        cursor.execute(organismelastid)
+                        conn.commit()
+                        organismelastid = cursor.fetchall()
+                        replacer3 = str(organismelastid[0])
+                        replace4 = replacer3.replace('(', "").replace \
+                            (',', "").replace(')', "")
+                        cursor.close()
+
+                        teller2 = int(replace4) + 1
+
+                        cursor = conn.cursor()
+                        string7 = f"insert into organisme (id, naam_organismenaam, " \
+                                  f"lineage_id) values ('{teller2}', '{datadic[i][1]}'" \
+                                  f", '{replace3}')"
+                        cursor.execute(string7)
+                        conn.commit()
+                        cursor.close()
+
+                        cursor = conn.cursor()
+                        sequentielastid = f"SELECT id FROM sequentie ORDER BY id DESC LIMIT 1"
+                        cursor.execute(sequentielastid)
+                        conn.commit()
+                        sequentielastid = cursor.fetchall()
+                        replacer4 = str(sequentielastid[0])
+                        replace5 = replacer4.replace('(', "").replace \
+                            (',', "").replace(')', "")
+                        cursor.close()
+
+                        teller3 = int(replace5) + 1
+
+                        numer = 3
+                        cursor = conn.cursor()
+                        # Zet de sequentie in de database
+                        string9 = f"insert into sequentie (id, header, sequence, " \
+                                  f"asci_score, _read_) values ('{teller3}', " \
+                                  f"'{datadic[i][8]}', '{datadic[i][7]}', " \
+                                  f"'{datadic[i][9]}', '{numer}')"
+                        cursor.execute(string9)
+                        conn.commit()
+                        cursor.close()
+
+                        seqidcursor = conn.cursor(buffered=True)
+                        string10 = f"select id from sequentie where header " \
+                                   f"= '{datadic[i][8]}'"
+                        seqidcursor.execute(string10)
+                        conn.commit()
+                        seqid = seqidcursor.fetchall()
+                        seqidcursor.close()
+                        replacer6 = str(seqid[0])
+                        replace7 = replacer6.replace('(', "").replace(',',
+                                                                      "").replace(
+                            ')', "")
+                        # Het id van organisme ophalen om deze in de
+                        # eiwit tabel te zetten
+                        orgidcursor = conn.cursor(buffered=True)
+                        string11 = f"select id from organisme where " \
+                                   f"naam_organismenaam = '{datadic[i][1]}'"
+                        orgidcursor.execute(string11)
+                        conn.commit()
+                        orgid = orgidcursor.fetchall()
+                        orgidcursor.close()
+                        replacer7 = str(orgid[0])
+                        replace8 = replacer7.replace('(', "").replace(',',
+                                                                      "").replace(
+                            ')', "")
+                        # De eiwit tabel vullen met data
+                        cursor = conn.cursor()
+                        string12 = f"insert into eiwit (id, description, " \
+                                   f"accessiecode, percent_identity, e_value, " \
+                                   f"max_score, total_score, query_cover, " \
+                                   f"sequentie_id, Organisme_id) values ('{i}'" \
+                                   f", '{datadic[i][0]}', '{datadic[i][2]}', " \
+                                   f"'{datadic[i][6]}', '{datadic[i][5]}', " \
+                                   f"'{datadic[i][3]}', '{datadic[i][4]}', " \
+                                   f"'{datadic[i][11]}', '{replace7}', '{replace8}')"
+                        cursor.execute(string12)
+                        conn.commit()
+                        cursor.close()
+
             else:
                 pass
             datalijst = []
